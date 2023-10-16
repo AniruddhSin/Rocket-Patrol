44da0bb77f4b28c87c1633f943a2531a0f621ac0
Aniruddh Sindhu <asindhu@ucsc.edu>
2023-10-15 22:45:03 -0700

    Added spaceships

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,19 @@
+//Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite{
+    constructor( scene, x, y, texture, frame, pointValue ){
+        super(scene,x,y,texture,frame)
+        scene.add.existing(this) //adds to scene
+        this.points = pointValue //point value of the ship
+        this.moveSpeed = 3       //pixels per frame
+    }
+
+    update(){
+        //spaceship always moves left
+        this.x -= this.moveSpeed
+
+        //if meets left edge, wrap around
+        if(this.x <= 0-this.width){
+            this.x = game.config.width
+        }
+    }
+}
