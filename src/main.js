6b6b48e81d44a98c116d294708cea2ffef92b5b9
Aniruddh Sindhu <asindhu@ucsc.edu>
2023-10-25 22:42:21 -0700

    Added Explanatory Comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+/*
+Aniruddh Sindhu
+Rocket Patrol Modular the IXth
+3 hours
+MODS:
+Change speed of game after 30 seconds - 1 point
+Control rocket after fired - 1 point
+Display the time remaining - 3 points
+Adds time to clock for successful hits - 5 points
+Add new spacehip that is smaller, faster, and worth more points - 5 points
+Explosion using particle emitter - 5 points
+
+*/
 //keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT
 
